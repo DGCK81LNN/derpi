c411cd4c5389807043f1864e1ee63e08254c6579
DGCK81LNN <triluolnn@163.com>
2022-01-22 20:08:44 +0800

    feat: NSFW content warning
    & js refactor

--- script.js
+++ script.js
@@ -1,40 +1,76 @@
-Array.prototype.forEach.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'), function (el) {
-  new bootstrap.Tooltip(el)
-})
+/**
+ * Shorthand for `document.getElementById`.
+ * @param {string} id
+ * @returns {HTMLElement}
+ */
+function $$$(id) {
+  return document.getElementById(id)
+}
 
-function $$$(id) { return document.getElementById(id) }
-
-function dismissWarning(index) {
-  var articleEl = $$$("result-" + index)
-  articleEl.classList.remove("result-spoilered")
-  var overlayEl = articleEl.querySelector(".result-spoilered-overlay")
-  if (overlayEl) overlayEl.hidden = true
-  articleEl.querySelector("img").removeAttribute("aria-hidden")
+/**
+ * Clone a template.
+ * This assumes that a template has only one child element.
+ * @param {string} id  the template's `id` without the `-template` postfix.
+ * @returns {HTMLElement}
+ */
+function cloneTemplate(id) {
+  return $$$(id + "-template").content.cloneNode(true).firstElementChild
 }
 
 $$$("search-submit").addEventListener("click", async function (event) {
   event.preventDefault()
-  var data = new FormData(this.form)
-  var query = "?" + [...data].map(e => e.map(s => encodeURIComponent(s)).join("=")).join("&")
-  try{
+  var data = [...new FormData(this.form)]
+  let queryParts = data.map(e => e.map(s => encodeURIComponent(s)).join("="))
+  var query = "?" + queryParts.join("&")
+
+  try {
     await search(query)
-  }catch(e){alert(e)}
+  } catch (err) {
+    alert("获取图片出错：" + err)
+    throw err
+  }
 })
 
+/** @type {{ [tagName: string]: string }} */
+const TAG_TRANSL = {
+  "suggestive": "性暗示",
+  "questionable": "强烈性暗示",
+  "explicit": "露骨性描写",
+  "semi-grimdark": "轻度黑暗",
+  "grimdark": "黑暗",
+  "grotesque": "血腥/恶心",
+  "seizure warning": "光敏癫痫警告",
+  "pony": "小马",
+  "human": "人类",
+  "anthro": "兽人",
+}
+/** @type {string[]} */
+const SPOILERED_TAGS = [
+  "suggestive",
+  "questionable",
+  "explicit",
+  "semi-grimdark",
+  "grimdark",
+  "grotesque",
+  "seizure warning",
+]
+
+// for all sizes see:
 // https://github.com/derpibooru/philomena/blob/cbbdaf677fef8fdd952e167fc91ff39a13b04c06/lib/philomena/images/thumbnailer.ex#L14-L23
-const IMG_SIZE_KEYWORD = "tall", IMG_MAX_WIDTH = 1024, IMG_MAX_HEIGHT = 4096
+const IMG_SIZE_KEYWORD = "medium",
+  IMG_MAX_WIDTH = 800,
+  IMG_MAX_HEIGHT = 600
 
 /** @param {string} query */
 async function search(query) {
   $$$("intro").hidden = true
   $$$("browse").hidden = false
   var containerEl = $$$("browse-images")
-  var templateEl = $$$("result-template").content
   document.body.scrollIntoView()
-  containerEl.classList.add("loading")
-  containerEl.setAttribute("aria-hidden", "true")
-  // containerEl.textContent = ""
-  // containerEl.appendChild($$$("loading-template").content.cloneNode(true))
+  // containerEl.classList.add("loading")
+  // containerEl.setAttribute("aria-hidden", "true")
+  containerEl.textContent = ""
+  containerEl.appendChild(cloneTemplate("loading"))
 
   var xhr = new XMLHttpRequest()
   xhr.open("get", "api.php" + query)
@@ -49,9 +85,8 @@ async function search(query) {
   containerEl.textContent = ""
   var response = JSON.parse(xhr.responseText)
   for (let result of response.images) {
-    var resultEl = templateEl.cloneNode(true)
-
-    var { width, height } = result
+    // calculate size of image preview
+    let { width, height } = result
     if (width >= height) {
       if (width > IMG_MAX_WIDTH) {
         height *= IMG_MAX_WIDTH / width
@@ -66,9 +101,38 @@ async function search(query) {
       }
     }
 
-    var img = resultEl.firstElementChild.firstElementChild
-    img.width = width, img.height = height
-    img.src = result.representations[IMG_SIZE_KEYWORD]
+    let resultEl = cloneTemplate("result")
+
+    let imgEl = resultEl.querySelector("img")
+    imgEl.width = width
+    imgEl.height = height
+    imgEl.src = result.representations[IMG_SIZE_KEYWORD]
+
+    if (result.spoilered) {
+      resultEl.classList.add("result-spoilered")
+      resultEl.ariaHidden = true
+
+      let overlayEl = cloneTemplate("result-spoiler")
+
+      let messageEl = overlayEl.querySelector(".result-spoilered-message")
+      SPOILERED_TAGS.forEach(tag => {
+        if (result.tags.includes(tag)) {
+          let tagEl = cloneTemplate("tag")
+          tagEl.querySelector(".tag-transl").textContent = TAG_TRANSL[tag]
+          tagEl.querySelector(".tag-name").textContent = tag
+          messageEl.append(" ", tagEl)
+        }
+      })
+
+      let buttonEl = overlayEl.querySelector(".result-spoilered-button")
+      buttonEl.addEventListener("click", () => {
+        resultEl.classList.remove("result-spoilered")
+        overlayEl.hidden = true
+        imgEl.ariaHidden = false
+      })
+
+      resultEl.append(overlayEl)
+    }
 
     containerEl.appendChild(resultEl)
   }
